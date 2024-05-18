c1006e5cc132b00332600d7eb11287d11f24016b
bryanleezh <leebryan307@gmail.com>
2024-05-18 15:43:03 +0800

    Add transitions

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,9 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "./theme-provider";
+import { ViewTransitions } from "next-view-transitions";
 
+// TODO: Add fonts in as variables to be used in typing component
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -16,20 +18,22 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body>
-        <ThemeProvider 
-          attribute="class" 
-          defaultTheme="system" 
-          enableSystem 
-          disableTransitionOnChange
-          themes={['light', 'dark', 'lightblue', 'darkblue']}
-        >
-          <main className={inter.className}>
-            {children}
-          </main>
-        </ThemeProvider>
-      </body>
-    </html>
+    <ViewTransitions>
+      <html lang="en" suppressHydrationWarning>
+        <body>
+          <ThemeProvider 
+            attribute="class" 
+            defaultTheme="system" 
+            enableSystem 
+            disableTransitionOnChange
+            themes={['light', 'dark', 'lightblue', 'darkblue']}
+            >
+            <main className={inter.className}>
+              {children}
+            </main>
+          </ThemeProvider>
+        </body>
+      </html>
+    </ViewTransitions>
   );
 }
