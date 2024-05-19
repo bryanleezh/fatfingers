437bba4ce8581c61bb3f1bbb79127232a0324d93
bryanleezh <leebryan307@gmail.com>
2024-05-19 22:48:02 +0800

    Add pages for navigation

--- app/info/page.tsx
+++ app/info/page.tsx
@@ -1,10 +1,11 @@
+
 import { Link } from "next-view-transitions";
 
-export default function Settings() {
+export default function Info() {
     return (
         <div className='flex flex-col items-center py-4 px-12'>
             <h2>
-                This is the <span className='settings'>Settings</span> Page
+                This is the <span className='info'>Info</span> Page
             </h2>
             <Link href='/'>Open homepage →</Link>
         </div>
