2b782ab2d63908d8cb2e1dfeb34138286171c255
bryanleezh <leebryan307@gmail.com>
2024-05-25 23:09:03 +0800

    Feat: Add custom ui components

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -18,7 +18,7 @@ const buttonVariants = cva(
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
-        texthighlight: "hover:bg-accent hover:text-primary hover:underline",
+        texthighlight: "text-muted-foreground hover:bg-accent hover:text-primary hover:underline",
       },
       size: {
         default: "h-10 px-4 py-2",
