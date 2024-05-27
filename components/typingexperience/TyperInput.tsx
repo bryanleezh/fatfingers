89c5004b942645e9393db7e676d875fde854abf2
bryanleezh <leebryan307@gmail.com>
2024-05-27 23:54:14 +0800

    Layered both text and input on top of each other

--- components/typingexperience/TyperInput.tsx
+++ components/typingexperience/TyperInput.tsx
@@ -6,7 +6,8 @@ import { Input } from "../ui/input";
 export default function TyperInput() {
     const time = useSettingsStore((state) => state.time);
     const mode = useSettingsStore((state) => state.mode);
-    const textinput = "hi welcome to fat fingers";
+    const textinput = "hi welcome to fat fingershi welcome to fat fingershi welcome to fat fingershi welcome to fat fingershi welcome to fat fingershi welcome to fat fingershi welcome to fat fingers hi welcome to fat fingershi welcome to fat fingershi welcome to fat fingershi welcome to fat fingershi welcome to fat fingershi welcome to fat fingershi welcome to fat fingers hi welcome to fat fingershi welcome to fat fingershi welcome to fat fingershi welcome to fat fingershi welcome to fat fingershi welcome to fat fingershi welcome to fat fingers hi welcome to fat fingershi welcome to fat fingershi welcome to fat fingershi welcome to fat fingershi welcome to fat fingershi welcome to fat fingershi welcome to fat fingers";
+    // const textinput = "hi welcome to fat fingers";
     
     // TODO: Create custom caret
     const startCaret = () => {
@@ -19,15 +20,16 @@ export default function TyperInput() {
     // TODO: Add separate cursor with smooth logic
     // TODO: Add scoring logic
     return (
-        <div className="flex flex-col items-center justify-center flex-grow">
-            <div className="">
-                <span className="text-primary leading-none">{textinput}</span>
+        <div className="w-screen max-w-screen-xl mx-auto relative h-1/2">
+            <div className="absolute inset-0 flex items-center justify-center">
+                <span className="text-primary text-2xl leading-none text-justify">{textinput}</span>
             </div>
-            <div className="">
+            <div className="absolute inset-0 flex items-center justify-center">
                 <Input 
-                    type="text" 
+                    type="text"
                     onFocus={startCaret}
-                    className="bg-background border-none text-primary caret-muted-foreground"
+                    className="bg-transparent border-none text-primary text-2xl caret-muted-foreground w-full h-full"
+                    // style={{ height: '100%'}}
                 />
             </div>
         </div>
