4debc8dd1d1a136e20e116f88bb9c22503098d58
bryanleezh <leebryan307@gmail.com>
2024-05-28 22:09:11 +0800

    Add TextWrapper + TextContainer components

--- components/typingexperience/TyperInput.tsx
+++ components/typingexperience/TyperInput.tsx
@@ -2,11 +2,13 @@
 
 import { useSettingsStore } from "@/store/settings";
 import { Input } from "../ui/input";
+import TextWrapper from "./TextWrapper";
+import TextContainer from "./TextContainer";
 
 export default function TyperInput() {
     const time = useSettingsStore((state) => state.time);
     const mode = useSettingsStore((state) => state.mode);
-    const textinput = "hi welcome to fat fingershi welcome to fat fingershi welcome to fat fingershi welcome to fat fingershi welcome to fat fingershi welcome to fat fingershi welcome to fat fingers hi welcome to fat fingershi welcome to fat fingershi welcome to fat fingershi welcome to fat fingershi welcome to fat fingershi welcome to fat fingershi welcome to fat fingers hi welcome to fat fingershi welcome to fat fingershi welcome to fat fingershi welcome to fat fingershi welcome to fat fingershi welcome to fat fingershi welcome to fat fingers hi welcome to fat fingershi welcome to fat fingershi welcome to fat fingershi welcome to fat fingershi welcome to fat fingershi welcome to fat fingershi welcome to fat fingers";
+    const textinput = "provided pish aboard without helpless crisp and exactly escalator yippee behind ack vector misconceive unabashedly per steep once gadzooks sight mmm throughout longingly vice appropriate versus desire plastic hm eek";
     // const textinput = "hi welcome to fat fingers";
     
     // TODO: Create custom caret
@@ -20,18 +22,10 @@ export default function TyperInput() {
     // TODO: Add separate cursor with smooth logic
     // TODO: Add scoring logic
     return (
-        <div className="w-screen max-w-screen-xl mx-auto relative h-1/2">
-            <div className="absolute inset-0 flex items-center justify-center">
-                <span className="text-primary text-2xl leading-none text-justify">{textinput}</span>
-            </div>
-            <div className="absolute inset-0 flex items-center justify-center">
-                <Input 
-                    type="text"
-                    onFocus={startCaret}
-                    className="bg-transparent border-none text-primary text-2xl caret-muted-foreground w-full h-full"
-                    // style={{ height: '100%'}}
-                />
-            </div>
+        <div className="w-full h-full mx-auto flex flex-col items-center justify-center max-w-5xl gap-4 px-4 xl:px-0">
+            <TextWrapper>
+                <TextContainer word={textinput}/>
+            </TextWrapper>
         </div>
     )
 };
