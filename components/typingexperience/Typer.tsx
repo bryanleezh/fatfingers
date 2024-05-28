de2da0d0f07e2fc7428c6f38157324fc40456188
bryanleezh <leebryan307@gmail.com>
2024-05-28 22:08:36 +0800

    Fix: Center TyperInput

--- components/typingexperience/Typer.tsx
+++ components/typingexperience/Typer.tsx
@@ -3,11 +3,12 @@
 import CustomizeBar from "./CustomizeBar";
 import TyperInput from "./TyperInput";
 
+// TODO: Add conditional for game start to replace CustomizeBar with timer
 export default function Typer() {
     return (
-        <div className="flex flex-col flex-grow gap-4 w-3/4">
+        <div className="flex flex-col gap-4 w-3/4 flex-grow items-center justify-center">
             <CustomizeBar />
-            <div className="flex flex-grow">
+            <div className="flex flex-grow items-center justify-center w-full">
                 <TyperInput />
             </div>
         </div>
