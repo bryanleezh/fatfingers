02ff1646d9dcae8c24935165c76d222bf2c92b9c
bryanleezh <leebryan307@gmail.com>
2024-06-03 14:59:35 +0800

    Fix: Fix styling

--- components/typingexperience/ResetGame.tsx
+++ components/typingexperience/ResetGame.tsx
@@ -6,7 +6,7 @@ type resetGameProps = {
 
 export default function ResetGame( { reset } : resetGameProps ) {
     return (
-        <div className="flex justify-center items-center">
+        <div className="flex justify-center items-center pt-3">
             <button className="rounded-full hover:bg-accent" onClick={reset}>
                 <RotateCcw className="text-primary text-2xl gap-3"/>
             </button>
