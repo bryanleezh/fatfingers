c43b2eaffd55bd40aaf630a96411c9a3b5c528eb
bryanleezh <leebryan307@gmail.com>
2024-06-13 23:28:01 +0800

    Add multiplayer pages

---
+++ app/multiplayer/[room]/page.tsx
@@ -0,0 +1,7 @@
+export default function MultiplayerRoom() {
+    return (
+        <>
+            Multiplayer Room
+        </>
+    )
+}
---
+++ app/multiplayer/page.tsx
@@ -0,0 +1,7 @@
+export default function Multiplayer() {
+    return (
+        <>
+            Multiplayer Home Page
+        </>
+    )
+}
