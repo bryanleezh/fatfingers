60e620df1959dfb67cd2efc1c4576dd6db052a23
bryanleezh <leebryan307@gmail.com>
2024-06-28 14:04:12 +0800

    Hide countdown on initial page load

--- components/multiplayer/CountDown.tsx
+++ components/multiplayer/CountDown.tsx
@@ -43,7 +43,7 @@ export default function CountDown({ countDown, onTimeUp} : CountDownProps) {
     }, [countDown, timer]);
 
     return (
-        <div className={`${countDown ? '' : ''} fixed bg-background rounded-lg border p-6 w-full max-w-md flex flex-col items-center gap-6`}>
+        <div className={`${countDown ? '' : 'hidden'} fixed bg-background rounded-lg border p-6 w-full max-w-md flex flex-col items-center gap-6`}>
             <div className="text-sm text-muted-foreground">Get ready...</div>
             <div className="text-sm text-muted-foreground text-center">Click on the blurred text to begin once this countdown ends!</div>
             <div className="text-4xl font-bold text-primary">
